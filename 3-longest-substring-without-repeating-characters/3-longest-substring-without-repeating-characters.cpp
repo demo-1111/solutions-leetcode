fd7b0ace6149cf1689490546b03cada32c25e8cb
demo-1111 <115419083+demo-1111@users.noreply.github.com>
2022-10-09 22:33:49 +0530

    Time: 3 ms (99.32%), Space: 6.7 MB (99.86%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,36 +1,37 @@
-class Solution {
-public:
-int lengthOfLongestSubstring(string &s)
+class Solution
 {
-    int res=1;
-    int n=s.length();
-    if(n==0) return 0;
-    int arr[256];
-    for(int i=0; i<256; i++)
-        arr[i]=-1;
-    arr[s[0]]=0;
-    int c=1,pre=0;
-    for(int i=1; i<n; i++) 
-    {
-        if(arr[s[i]]==-1)
+    public:
+        int lengthOfLongestSubstring(string & s)
         {
-            arr[s[i]]=i;
-            c++;
-            res=max(res,c);
+            int res = 1;
+            int n = s.length();
+            if (n == 0) return 0;
+            int arr[256];
+            for (int i = 0; i < 256; i++)
+                arr[i] = -1;
+            arr[s[0]] = 0;
+            int c = 1, pre = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[s[i]] == -1)
+                {
+                    arr[s[i]] = i;
+                    c++;
+                    res = max(res, c);
+                }
+                else
+                {
+                    int loc = arr[s[i]];
+                    for (int k = pre; k <= loc; k++)
+                        arr[s[k]] = -1;
+                    c = i - loc;
+                    pre = loc + 1;
+                   	//             gg
+                    arr[s[i]] = i;
+                }
+               	// cout<<res<<" ";
+            }
+           	// cout<<"\n";
+            return res;
         }
-        else
-        {
-            int loc=arr[s[i]];
-            for(int k=pre; k<=loc; k++) 
-                arr[s[k]]=-1;
-            c=i-loc;
-            pre=loc+1;
-//             gg
-            arr[s[i]]=i;
-        }
-        // cout<<res<<" ";
-    }
-    // cout<<"\n";
-    return res;
-}
 };
