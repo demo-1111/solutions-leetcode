550bdf865a6f913dcdd11f676b838e69cb438326
demo-1111 <115419083+demo-1111@users.noreply.github.com>
2022-10-11 16:59:54 +0530

    Time: 78 ms (89.39%), Space: 61.9 MB (14.42%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+    void ceil(int tail[], int x, int len)
+    {
+        int i=0, j=len-1, ind=-1;
+        while(i<=j)
+        {
+            int mid=(i+j)/2;
+            if(tail[mid]==x) return;
+            else if(tail[mid]<x) i=mid+1;
+            else
+            {
+                ind=mid;
+                j=mid-1;
+            }
+        }
+        if(ind!=-1)
+            tail[ind]=x;
+    }
+    bool increasingTriplet(vector<int> &a)
+    {
+        int n = a.size();
+        int tail[n];
+        int len=0;
+        tail[len++]=a[0];
+        for(int j=1; j<n; j++)
+        {
+            if(tail[len-1]<a[j])
+                tail[len++]=a[j];
+            else
+                ceil(tail, a[j], len);
+        }
+        if (len >= 3) return true;
+        return false;
+    }
+};
