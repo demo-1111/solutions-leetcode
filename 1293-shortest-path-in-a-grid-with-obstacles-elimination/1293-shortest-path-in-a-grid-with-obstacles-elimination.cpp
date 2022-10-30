14e33cd42ae1fa1690e2a90b841ef903c7cbfe1f
demo-1111 <115419083+demo-1111@users.noreply.github.com>
2022-10-31 02:12:22 +0530

    Time: 89 ms (75.88%), Space: 15.3 MB (77.66%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,67 @@
+struct triple
+{
+    int i,j,z;
+    triple(int i, int j , int z):i(i),j(j),z(z){}
+};
+class Solution {
+public:
+
+int shortestPath(vector<vector<int>>& grid, int k)
+{
+    int n=grid.size(), m=grid[0].size(), res=0;
+    if(n==1 && m==1) return 0;
+    if(grid[0][0]==1) k--;
+    bool visited[n][m][k+1];
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            for(int z=0; z<k+1; z++)
+                visited[i][j][z]=false;
+    queue<triple> q;
+    q.push({0,0,k});
+    visited[0][0][k]=true;
+    int ar1[4]={1,-1,0,0};
+    int ar2[4]={0,0,-1,1};
+    while(!q.empty())
+    {
+        int sz=q.size();
+        for(int i=0; i<sz; i++) 
+        {
+            int ci=q.front().i;
+            int cj=q.front().j;
+            int ck=q.front().z;
+            q.pop();
+            for(int u=0; u<4; u++) 
+            {
+                int ti=ci+ar1[u];
+                int tj=cj+ar2[u];
+                if(ti>=0 && ti<n && tj>=0 && tj<m)
+                {
+                    if(visited[ti][tj][ck]==false)
+                    {
+                        if(ti==n-1 && tj==m-1)
+                            return res+1;
+                        if(grid[ti][tj]==0)
+                        {
+                            q.push({ti,tj,ck});
+                            visited[ti][tj][ck]=true;
+                        }
+                    }
+                    if(ck>=1 && visited[ti][tj][ck-1]==false)
+                    {
+                        if(ti==n-1 && tj==m-1)
+                            return res+1;
+                        if(ck>=1 && grid[ti][tj]==1)
+                        {
+                            q.push({ti,tj,ck-1});
+                            visited[ti][tj][ck-1]=true;
+                        }                        
+                    }
+                }
+            }
+        }
+        res++;
+    }
+    return -1;
+}
+
+};
