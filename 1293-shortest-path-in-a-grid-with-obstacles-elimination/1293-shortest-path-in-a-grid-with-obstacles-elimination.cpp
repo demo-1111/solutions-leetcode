22cb68761d2077d1b7332ea915139d383e70cd93
demo-1111 <115419083+demo-1111@users.noreply.github.com>
2022-10-31 02:16:10 +0530

    Time: 112 ms (71.43%), Space: 15.3 MB (77.66%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -36,10 +36,12 @@ int shortestPath(vector<vector<int>>& grid, int k)
                 int tj=cj+ar2[u];
                 if(ti>=0 && ti<n && tj>=0 && tj<m)
                 {
-                    if(visited[ti][tj][ck]==false)
-                    {
                         if(ti==n-1 && tj==m-1)
                             return res+1;
+                    if(visited[ti][tj][ck]==false)
+                    {
+                        // if(ti==n-1 && tj==m-1)
+                        //     return res+1;
                         if(grid[ti][tj]==0)
                         {
                             q.push({ti,tj,ck});
@@ -48,8 +50,8 @@ int shortestPath(vector<vector<int>>& grid, int k)
                     }
                     if(ck>=1 && visited[ti][tj][ck-1]==false)
                     {
-                        if(ti==n-1 && tj==m-1)
-                            return res+1;
+                        // if(ti==n-1 && tj==m-1)
+                        //     return res+1;
                         if(grid[ti][tj]==1)
                         {
                             q.push({ti,tj,ck-1});
