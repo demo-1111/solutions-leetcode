6cf0e623596bf2ff4b1731d95428ec5941f4769f
demo-1111 <115419083+demo-1111@users.noreply.github.com>
2022-10-30 16:07:56 +0530

    Time: 4 ms (59.98%), Space: 5.9 MB (24.80%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
+// int mySqrt(int x)
+// {
+//     unsigned long long int i=0;
+//     while(i*i <= x)
+//         i++;
+//     return i-1;
+// }
+    
 int mySqrt(int x)
 {
-    unsigned long long int i=0;
-    while(i*i <= x)
-        i++;
-    return i-1;
+    unsigned long long res=0, l=0, r=x, m, cur;
+    while(l<=r)
+    {
+        m=l+(r-l)/2;
+        cur=m*m;
+        if(cur==x) return m;
+        else if(cur<x)
+        {
+            l=m+1;
+            res=m;
+        }
+        else
+            r=m-1;
+    }
+    return res;
 }
 
 };
